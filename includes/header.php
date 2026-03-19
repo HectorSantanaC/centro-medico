36d8e75e00b41a85f931538456acddba0a4cbf5c
Héctor Santana <hector.santana.gc@gmail.com>
2026-03-19 20:44:10 +0000

    Add CRUD citas y fix ruta CSS para compatibilidad local

--- includes/header.php
+++ includes/header.php
@@ -5,13 +5,13 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Centro Médico TAC7 - <?php echo $page_title ?? 'Inicio'; ?></title>
-  <link rel="stylesheet" href="/css/style.css">
+  <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
   <header>
     <nav class="navbar">
-      <h5 class="logo">CENTRO MÉDICO TAC7</h5>
+      <h5 class="logo"><a href="index.php">CENTRO MÉDICO TAC7</a></h5>
 
       <div>
         <ul class="menu">
