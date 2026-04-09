1503944584b4dcecbe79dbec7ddd4271f2956525
Héctor Santana <hector.santana.gc@gmail.com>
2026-04-09 20:48:03 +0100

    Add link to blog.php in index

--- index.php
+++ index.php
@@ -138,7 +138,7 @@ include './views/layout/header.php';
   <article class="publicaciones">
     <h2>Noticias & Publicaciones</h2>
     <h3>Actualidad, consejos de salud y publicaciones de nuestros profesionales para tu bienestar integral.</h3>
-    <a href="#">Visita el blog
+    <a href="blog.php">Visita el blog
       <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-arrow-narrow-right">
         <path stroke="none" d="M0 0h24v24H0z" fill="none" />
         <path d="M5 12l14 0" />
