75f2883b83277994a4120fd3dc5666a9a06ce9bc
Héctor Santana <hector.santana.gc@gmail.com>
2026-04-16 20:59:31 +0100

    fix(admin): crear archivos de entrada y corregir rutas de assets

--- views/admin/especialidades.php
+++ views/admin/especialidades.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Gestión de Especialidades</title>
-  <link rel="stylesheet" href="css/admin.css">
+  <link rel="stylesheet" href="../css/admin.css">
 </head>
 
 <body>
@@ -71,7 +71,7 @@
 
   <?php require_once __DIR__ . '/../layout/footer-admin.php'; ?>
 
-  <script src="js/admin-especialidades.js"></script>
+  <script src="../js/admin-especialidades.js"></script>
 </body>
 
 </html>
