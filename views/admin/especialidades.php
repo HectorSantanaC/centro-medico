215d404f07ee3b952cb4c164e2d3d04ad4300d66
Héctor Santana <hector.santana.gc@gmail.com>
2026-04-16 21:17:29 +0100

    fix(admin): corregir rutas de CSS y JS en vistas

--- views/admin/especialidades.php
+++ views/admin/especialidades.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Gestión de Especialidades</title>
-  <link rel="stylesheet" href="../css/admin.css">
+  <link rel="stylesheet" href="css/admin.css">
 </head>
 
 <body>
@@ -69,7 +69,7 @@
     </div>
   </div>
 
-  <script src="../js/admin-especialidades.js"></script>
+  <script src="js/admin-especialidades.js"></script>
 </body>
 
 </html>
--- views/admin/usuarios.php
+++ views/admin/usuarios.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Gestión de Usuarios</title>
-  <link rel="stylesheet" href="../css/admin.css">
+  <link rel="stylesheet" href="css/admin.css">
 </head>
 
 <body>
@@ -89,7 +89,7 @@
     </div>
   </div>
 
-  <script src="../js/admin-usuarios.js"></script>
+  <script src="js/admin-usuarios.js"></script>
 </body>
 
 </html>
