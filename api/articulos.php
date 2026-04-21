1a7e4678ac49eba37d0d06f0952a62e82bee977e
Héctor Santana <hector.santana.gc@gmail.com>
2026-04-21 18:41:28 +0100

    Fix: inicializar modelo en API REST de articulos

--- api/articulos.php
+++ api/articulos.php
@@ -2,6 +2,8 @@
 
 require_once __DIR__ . '/../models/Articulo.php';
 
+$articuloModel = new Articulo();
+
 header('Content-Type: application/json; charset=utf-8');
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
